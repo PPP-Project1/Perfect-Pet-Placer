0d5da70f379c7540aba2784858a1909ee00a6bf0
daniel <yoojeenkim@gmail.com>
2021-09-29 11:47:34 -0400

    working fetch request check for user input

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,9 +1,15 @@
 var APIKey = "";
 var APIsecret = "";
-var org = 'RI77';
-var status = 'adoptable';
+var type;
+var age;
+var gender;
+var size;
+var coat;
+var address;
+var distance;
 
-function getAccessToken () {
+function fetchPetAPI () {
+    //this fetch call retrieves access token for user to use for 1 hour
     fetch('https://api.petfinder.com/v2/oauth2/token', {
         method: 'POST',
         body: 'grant_type=client_credentials&client_id=' + APIKey + '&client_secret=' + APIsecret,
@@ -15,8 +21,9 @@ function getAccessToken () {
     }).then(function(data) {
         console.log('token',data);
 
-        //from this point on, is purely testing.. lines 19 - 35 will be a seperate function where user will input parameters to the url
-        return fetch('https://api.petfinder.com/v2/animals?organization=' + org + '&status=' + status, {
+        //this fetch call will retrieve results for pet information based on user input
+        return fetch('https://api.petfinder.com/v2/animals?type=' + type + '&age=' + age + '&gender=' + gender + '&size=' + size + '&coat=' + coat + '&address=' + address + 'distance' + distance, {
+            method: 'GET',
             headers: {
                 'Authorization': data.token_type + ' ' + data.access_token,
                 'Content-Type': 'application/x-www-form-urlencoded'
@@ -28,10 +35,11 @@ function getAccessToken () {
     }).then(function (data) {
         console.log('pets', data);
 
+    //catch function to console log any errors that may occur
     }).catch(function (err) {
         console.log('something went wrong', err);
 
     });
 }
 
-getAccessToken();
+fetchPetAPI();
