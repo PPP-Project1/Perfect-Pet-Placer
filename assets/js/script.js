25d4a0f7964b6f4922fa4ad1bde0c2c7c77fec44
daniel <yoojeenkim@gmail.com>
2021-09-30 00:35:25 -0400

    refactored code

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,14 +1,10 @@
-var APIKey = "iwfBo0lysmRDywH7YnUK8MqtITZWVbmzDeYpUuFE5cIJhzelM7";
-var APIsecret = "oH97v5MQBen8II1y33uDrSab6xa8NRxhDwpmx9lS";
-var type ;
-// var age;
-// var gender;
-// var size;
-// var coat;
-// var address;
-// var distance;
+//carousel 
+$('.carousel').carousel({ interval: 2000 });
 
-function fetchPetAPI () {
+function fetchToken () {
+    var APIKey = "iwfBo0lysmRDywH7YnUK8MqtITZWVbmzDeYpUuFE5cIJhzelM7";
+    var APIsecret = "oH97v5MQBen8II1y33uDrSab6xa8NRxhDwpmx9lS";
+    
     //this fetch call retrieves access token for user to use for 1 hour
     fetch('https://api.petfinder.com/v2/oauth2/token', {
         method: 'POST',
@@ -18,38 +14,30 @@ function fetchPetAPI () {
         }
     }).then(function(resp) {
         return resp.json();
-    }).then(function(data) {
-        console.log('token',data);
+        
+    }).then(function(token) {
+        return fetchPetAPI(token);
 
-        //this fetch call will retrieve results for pet information based on user input
-        return fetch('https://api.petfinder.com/v2/animals?type=' + type, {
+    })
+}
+
+function fetchPetAPI (token) {
+
+    //this fetch call will retrieve results for pet information based on user input
+    fetch('https://api.petfinder.com/v2/animals?type=' + type, {
             method: 'GET',
             headers: {
-                'Authorization': data.token_type + ' ' + data.access_token,
+                'Authorization': 'Bearer '+ token.access_token,
                 'Content-Type': 'application/x-www-form-urlencoded'
             }
-        });
     }).then(function (resp) {
-
         return resp.json();
+
     }).then(function (data) {
         console.log('pets', data);
 
-    //catch function to console log any errors that may occur
-    }).catch(function (err) {
-        console.log('something went wrong', err);
-
     });
 }
-
-
-// Carousel 
-
-$('.carousel').carousel({ interval: 2000 });
-
-
-fetchPetAPI();
-
 
 function searchHandler(event){
     event.preventDefault();
@@ -74,16 +62,14 @@ function searchHandler(event){
     }
     
     if(!address){
-            console.error("Please enter your location.")
-        }
+        console.error("Please enter your location.")
+    }
         
-        // var querySearch = "./results.html?q+" + type + "&gender=" + gender + "&age=" + age + "&size=" + size + "&coatLength=" + 
-        // coatLength + "&address=" + address + "&distance=" + distance;
+        //var querySearch = "./results.html?q+" + type + "&gender=" + gender + "&age=" + age + "&size=" + size + "&coatLength=" + 
+        //coatLength + "&address=" + address + "&distance=" + distance;
         
-        // location.assign(querySearch);
+        //location.assign(querySearch);
         
     }
 
-    fetchPetAPI();
-    $("#submitBtn").click(searchHandler);
-
+$("#submitBtn").click(searchHandler);
