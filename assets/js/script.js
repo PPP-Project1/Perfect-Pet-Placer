24fc7d22d1fd1a5a3824bfa59f3333d61463728d
Bryanna Vinge <bevinge711@gmail.com>
2021-09-29 21:08:25 -0500

    mapAPI call still in progress, trying to decipher how to use variable address in call

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,14 +1,15 @@
 var APIKey = "iwfBo0lysmRDywH7YnUK8MqtITZWVbmzDeYpUuFE5cIJhzelM7";
 var APIsecret = "oH97v5MQBen8II1y33uDrSab6xa8NRxhDwpmx9lS";
-var type ;
+// var type;
 // var age;
 // var gender;
 // var size;
 // var coat;
-// var address;
+// var address; - this may still want to be location
 // var distance;
 
-function fetchPetAPI () {
+
+function fetchPetAPI() {
     //this fetch call retrieves access token for user to use for 1 hour
     fetch('https://api.petfinder.com/v2/oauth2/token', {
         method: 'POST',
@@ -16,10 +17,10 @@ function fetchPetAPI () {
         headers: {
             'Content-Type': 'application/x-www-form-urlencoded'
         }
-    }).then(function(resp) {
+    }).then(function (resp) {
         return resp.json();
-    }).then(function(data) {
-        console.log('token',data);
+    }).then(function (data) {
+        console.log('token', data);
 
         //this fetch call will retrieve results for pet information based on user input
         return fetch('https://api.petfinder.com/v2/animals?type=' + type, {
@@ -35,12 +36,31 @@ function fetchPetAPI () {
     }).then(function (data) {
         console.log('pets', data);
 
-    //catch function to console log any errors that may occur
+        //catch function to console log any errors that may occur
     }).catch(function (err) {
         console.log('something went wrong', err);
 
     });
 }
+
+var apiKey2 = "AIzaSyAnFzh7TbHHX423_Cve8xpaB3sWJ05-rO8";
+var rescueAddress; // This may be pulled from petfinder API as: Address1 + City+ State
+var geoURL = "https://maps.googleapis.com/maps/api/geocode/json?address=" + rescueAddress + "&key=" + apiKey2;
+
+function fetchGoogleApi() {
+    fetch(geoURL)
+        .then(function (res) {
+            return res.json();
+        })
+        .then(function (data) {
+            console.log(data)
+        })
+}; 
+
+var lon1 = data.results.location.lng;
+var lat1 = data.results.location.lat;
+
+
 
 
 // Carousel 
@@ -51,7 +71,7 @@ $('.carousel').carousel({ interval: 2000 });
 fetchPetAPI();
 
 
-function searchHandler(event){
+function searchHandler(event) {
     event.preventDefault();
     
     var type = document.getElementById("animalType").value;
@@ -68,22 +88,25 @@ function searchHandler(event){
     console.log(address)
     var distance = document.getElementById("distance").value;
     console.log(distance)
-
-    if(!type){
+    
+    if (!type) {
         console.error("Please select a type.")
     }
     
-    if(!address){
-            console.error("Please enter your location.")
-        }
-        
-        // var querySearch = "./results.html?q+" + type + "&gender=" + gender + "&age=" + age + "&size=" + size + "&coatLength=" + 
-        // coatLength + "&address=" + address + "&distance=" + distance;
-        
-        // location.assign(querySearch);
-        
+    if (!address) {
+        console.error("Please enter your location.")
     }
+    
+    // var querySearch = "./results.html?q+" + type + "&gender=" + gender + "&age=" + age + "&size=" + size + "&coatLength=" + 
+    // coatLength + "&address=" + address + "&distance=" + distance;
+    
+    // location.assign(querySearch);
+    
+}
+fetchPetAPI();
+$("#submitBtn").click(searchHandler);
 
-    fetchPetAPI();
-    $("#submitBtn").click(searchHandler);
+
+
+
 
